4bdd0db4ad58e14dc9dc5d2183188ff12eb84bbc
Robert Pietrzykowski <robert.pietrzykowski@pierce-ecom.com>
2020-01-31 12:56:52 +0100

    delete token

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# egnyte
+# Egnyte Backend Test Cases
+
+Before launch test please add token in login_resources.py
--- resources/login_resources.py
+++ resources/login_resources.py
@@ -2,6 +2,6 @@ class LoginCredentials:
     # Login credentials fo API
     CLIENT_ID = '5061296246b040fc89b3e06e75271439'
     USERNAME = 'admin'
-    PASSWORD = '50612962'
+    PASSWORD = ''
     GRANT_TYPE = 'password'
-    TOKEN = 'jpcxxdxfqja2nxfmvt5r8bmu'
+    TOKEN = ''
